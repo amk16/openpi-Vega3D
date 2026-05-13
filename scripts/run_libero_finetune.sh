10f28df59b12961425355c9f0c6b464b508edd9c
Salman Shahid <salmanshahidzia@gmail.com>
2026-05-13 20:00:41 +0000

    training kick off script for pi05_libero_lora

---
+++ scripts/run_libero_finetune.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Download dataset
+NVME_DIR=/workspace
+HF_HOME="$NVME_DIR/.hf_home"
+echo $HF_HOME
+aws s3 sync s3://behavior-challenge/lerobot/ "$HF_HOME/lerobot/"
+
+# Activate venv
+source /venv/main/bin/activate
+
+# Kick off training run
+HF_HOME=$HF_HOME XLA_PYTHON_CLIENT_MEM_FRACTION=0.95 uv run scripts/train.py \
+    pi05_libero_lora \
+    --exp-name=pi05_libero_lora_base \
+    --overwrite
--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -852,6 +852,34 @@ _CONFIGS = [
         pytorch_weight_path="/path/to/your/pytorch_weight_path",
         num_train_steps=30_000,
     ),
+    TrainConfig(
+        name="pi05_libero_lora",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
+        data=LeRobotLiberoDataConfig(
+            repo_id="physical-intelligence/libero",
+            base_config=DataConfig(prompt_from_task=True),
+            extra_delta_transform=False,
+            # normalization_stats_path="assets/pi05_libero/physical-intelligence/libero/norm_stats.json",
+            assets=AssetsConfig(
+                assets_dir="/workspace/openpi-Vega3D/assets/pi05_libero",
+                asset_id=None,
+            ),
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        num_train_steps=30_000,
+        batch_size=256,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            warmup_steps=1_000,
+            peak_lr=5e-5,  # TODO: remember to decrease if lowering batch size
+            decay_steps=30_000,
+            decay_lr=5e-6,  # TODO: remember to decrease if lowering batch size
+        ),
+        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
+        freeze_filter=pi0_config.Pi0Config(
+            paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
+        ).get_freeze_filter(),
+        ema_decay=None,
+    ),
     #
     # Fine-tuning Aloha configs.
     #
